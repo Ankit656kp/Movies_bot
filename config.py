0e812ba656fc4284c1ca27c2833817891edcb1cf
Ankit kumar <ankitgupta2ndsp@gmail.com>
2025-07-14 13:26:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
+OWNER_ID = int(os.getenv("OWNER_ID"))
